90def0d47238945ff698b91381ca60692e8ea031
f15freedomeagle <tridung9679@gmail.com>
2026-01-15 11:57:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const MODEL_MAPPING = {
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
   'claude-3-opus': 'deepseek-ai/deepseek-v3.2',
-  'claude-3-sonnet': 'openai/gpt-oss-20b',
+  'claude-3-sonnet': 'mistralai/mistral-large-3-675b-instruct-2512',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking' 
 };
 
