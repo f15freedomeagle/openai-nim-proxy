eb78b3f9cf47628931af957bc7445254d9cdf21f
f15freedomeagle <tridung9679@gmail.com>
2026-04-26 16:34:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'mistralai/mistral-small-4-119b-2603',
   'gpt-4': 'qwen/qwen3.5-122b-a10b',
   'gpt-4-turbo': 'moonshotai/kimi-k2-instruct-0905',
-  'gpt-4o': 'deepseek-ai/deepseek-v3.2',
+  'gpt-4o': 'minimaxai/minimax-m2.5',
   'claude-3-opus': 'nvidia/nemotron-3-super-120b-a12b',
   'claude-3-sonnet': 'mistralai/devstral-2-123b-instruct-2512'
 };
