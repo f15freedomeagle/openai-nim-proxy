9f11849a13ca049cd0b3da9f8d5ee3412cc150e3
f15freedomeagle <tridung9679@gmail.com>
2026-05-16 23:20:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 
 // Model mapping (maps OpenAI model names to NVIDIA models)
 const MODEL_MAPPING = {
-  'gpt-3.5-turbo': 'mistralai/mistral-large-3-675b-instruct-2512',
+  'gpt-3.5-turbo': 'mistralai/mistral-small-4-119b-2603',
   'gpt-4': 'qwen/qwen3-next-80b-a3b-instruct',
   'gpt-4-turbo': 'deepseek-ai/deepseek-v4-pro',
   'gpt-4o': 'mistralai/mistral-large-3-675b-instruct-2512',
