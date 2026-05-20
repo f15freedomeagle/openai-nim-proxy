10d1f8ba6b943e28efce21814fcfe3bf74895439
f15freedomeagle <tridung9679@gmail.com>
2026-05-20 17:28:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const MODEL_MAPPING = {
   'gpt-4': 'qwen/qwen3-next-80b-a3b-instruct',
   'gpt-4-turbo': 'deepseek-ai/deepseek-v4-pro',
   'gpt-4o': 'nvidia/llama-3.3-nemotron-super-49b-v1.5',
-  'claude-3-opus': 'nvidia/nemotron-3-nano-30b-a3b',
+  'claude-3-opus': 'mistralai/mistral-medium-3.5-128b',
   'claude-3-sonnet': 'mistralai/ministral-14b-instruct-2512'
   };
 
