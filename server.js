6741c8f1ecede446d3dcafd60f7affa3c816d2ac
f15freedomeagle <tridung9679@gmail.com>
2026-06-05 20:18:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'deepseek-ai/deepseek-v4-flash',
   'gpt-4': 'qwen/qwen3-next-80b-a3b-instruct',
-  'gpt-4-turbo': 'google/gemma-3n-e4b-it',
+  'gpt-4-turbo': 'qwen/qwen3.5-122b-a10b',
   'gpt-4o': 'mistralai/mistral-small-4-119b-2603',
   'claude-3-opus': 'mistralai/mistral-medium-3.5-128b',
   'claude-3-sonnet': 'mistralai/ministral-14b-instruct-2512'
