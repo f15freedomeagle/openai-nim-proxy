98a6bd9665c2e89642d413620a44ad3fd5459c7c
f15freedomeagle <tridung9679@gmail.com>
2026-07-04 02:10:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 
 // Model mapping (maps OpenAI model names to NVIDIA models)
 const MODEL_MAPPING = {
-  'gpt-3.5-turbo': 'google/diffusiongemma-26b-a4b-it',
+  'gpt-3.5-turbo': 'z-ai/glm-5.2',
   'gpt-4': 'qwen/qwen3-next-80b-a3b-instruct',
   'gpt-4-turbo': 'qwen/qwen3.5-122b-a10b',
   'gpt-4o': 'mistralai/mistral-small-4-119b-2603',
